c1ed8c043458c6043022d670fae20820472881dc
GF63 <GF63@MSI>
2026-03-01 21:51:29 -0400

    Agregar archivos de proyecto.

---
+++ DATOS/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+
+namespace DATOS
+{
+    public class ConexionSql
+    {
+        private SqlConnection conexion = new SqlConnection("Server=.;Database=CENTRORECARGA;Integrated Security=True");
+
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+    }
+}
---
+++ NEGOCIO/Recarga.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+using DATOS;
+
+namespace NEGOCIO
+{
+    public class CN_Recarga
+    {
+        private CD_ConsultaRecarga consulta = new CD_ConsultaRecarga();
+
+        public DataTable MostrarRecargas()
+        {
+            DataTable tabla = new DataTable();
+            tabla = consulta.Mostrar();
+            return tabla;
+        }
+    }
+}
